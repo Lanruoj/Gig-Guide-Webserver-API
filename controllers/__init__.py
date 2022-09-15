0f61f7b0de82c5d343696dea10f171bdc47716f9
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-09-15 17:20:40 +1000

    artist_controller, +show_all_artists

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,6 +1,6 @@
 from controllers.auth_controller import auth
 from controllers.gig_controller import gigs
-# from controllers.artist_controller import artists
+from controllers.artist_controller import artists
 # from controllers.venue_controller import venues
 
-registerable_controllers = [auth, gigs]
+registerable_controllers = [auth, gigs, artists]
---
+++ controllers/artist_controller.py
@@ -0,0 +1,19 @@
+from main import db, bcrypt, jwt
+from flask import Blueprint, jsonify, request, abort
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from datetime import datetime
+from models.gig import Gig
+from schemas.gig_schema import gig_schema, gigs_schema
+from models.performance import Performance
+from schemas.performance_schema import performance_schema
+from models.artist import Artist
+from schemas.artist_schema import artist_schema, artists_schema
+
+
+artists = Blueprint("artists", __name__, url_prefix="/artists")
+
+
+@artists.route("/", methods=["GET"])
+def show_all_artists():
+    artist_list = Artist.query.all()
+    return jsonify(artists_schema.dump(artist_list))

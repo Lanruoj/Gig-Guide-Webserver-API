0c300cc3622f776e540b9254c9a2e7070d990aa9
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-09-29 14:47:09 +1000

    ArtistSchema validation, UserSchema validation

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -8,12 +8,12 @@ class UserSchema(ma.Schema):
         ordered = True
         fields = ("user_id", "username", "email", "password", "first_name", "last_name", "logged_in", "watched_venues", "watched_artists")
     
-    # username = ma.String(required=True, )
-    password = ma.String(validate=Length(min=8))
-    username = ma.String(required=True)
+    username = ma.String(required=True, validate=Length(min=3, max=10))
+    password = ma.String(required=True, validate=Length(min=8))
     email = ma.Email(required=True)
+    first_name = ma.String(required=True)
+    last_name = ma.String(required=True)
 
-    #######
     watched_venues = fields.List(fields.Nested("WatchVenueSchema"))
     watched_artists = fields.List(fields.Nested("WatchArtistSchema"))
 

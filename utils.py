c1a82ec1047ff3ae490972085d742f3855273285
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-10-04 10:59:34 +1100

    search() works for int values such as price=20, checks if arg in table.columns

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from flask import request, abort, jsonify
+from flask import request, abort, jsonify, Markup
 
 
 def search(table, schema, filters=None, sort=None, no_results="No results found"):
@@ -7,16 +7,22 @@ def search(table, schema, filters=None, sort=None, no_results="No results found"
     if request.query_string:
         no_results = "No results matching that criteria"
         for arg in request.args:
-            if arg != "sort":
-                if "id" in arg:
+            if arg not in vars(table) and arg != "sort":
+                return abort(400, description=Markup(f"The keyword '{arg}' is not valid search criteria"))
+
+            if arg == "sort":
+                sort = getattr(table, request.args[arg])
+
+            elif arg != "sort":
+                # IF SEARCHING FOR A NUMERIC VALUE
+                if "id" in arg or request.args[arg].isdigit():
                     _filter = getattr(table, arg) == request.args[arg]
                     filters.append(_filter)
+                # IF SEARCHING FOR A STRING VALUE
                 else:
                     _filter = getattr(table, arg).ilike(f"%{request.args[arg]}%")
                     filters.append(_filter)
 
-            elif arg == "sort":
-                sort = getattr(table, request.args[arg])
 
     results = table.query.filter(*(filters)).order_by(sort).all()
     if not results:
